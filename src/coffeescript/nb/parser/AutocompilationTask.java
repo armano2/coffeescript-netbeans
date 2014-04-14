f6d18ee0d00cee48c16dac2a0b0843bce25b1712
milos <milos@milos-R500>
2014-04-14 17:46:12 +0200

    * new autocompilation task for legacy and new parser.

---
+++ src/coffeescript/nb/parser/AutocompilationTask.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package coffeescript.nb.parser;
+
+import coffeescript.nb.core.CoffeeScriptAutocompileContext;
+import coffeescript.nb.core.CoffeeScriptCompiler;
+import coffeescript.nb.core.CoffeeScriptUtils;
+import coffeescript.nb.options.CoffeeScriptSettings;
+import org.netbeans.modules.parsing.api.Snapshot;
+import org.openide.filesystems.FileObject;
+
+/**
+ *
+ * @author Miloš Pensimus
+ */
+public class AutocompilationTask implements Runnable {
+    private final Snapshot snapshot;
+
+    public AutocompilationTask(Snapshot snapshot) {
+        this.snapshot = snapshot;
+    }
+
+    public void run() {
+        if(!CoffeeScriptAutocompileContext.get().isEnabled(snapshot.getSource().getFileObject())) return;
+        CharSequence text = snapshot.getText();
+        CoffeeScriptCompiler.CompilerResult compilerResult = CoffeeScriptSettings.getCompiler().compile(text.toString(), CoffeeScriptSettings.get().isBare());
+        if ((compilerResult != null) && (compilerResult.getJs() != null)) {
+            FileObject coffeeFile = snapshot.getSource().getFileObject();
+            String js = compilerResult.getJs();
+            CoffeeScriptUtils.writeJSForCoffeeScriptFile(js, coffeeFile);
+        }
+    }
+    
+}
