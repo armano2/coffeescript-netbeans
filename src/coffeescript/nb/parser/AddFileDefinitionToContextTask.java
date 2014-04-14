d60aa522afe397e10404ded037c30df0e540be84
milos <milos@milos-R500>
2014-04-14 17:39:55 +0200

    * created task that puts coffeescript file definitions to global lookup context

---
+++ src/coffeescript/nb/parser/AddFileDefinitionToContextTask.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coffeescript.nb.parser;
+
+import coffeescript.nb.core.CoffeeScriptDataObject;
+import coffeescript.nb.core.context.ICoffeeScriptMainContext;
+import org.netbeans.modules.parsing.spi.ParserResultTask;
+import org.netbeans.modules.parsing.spi.Scheduler;
+import org.netbeans.modules.parsing.spi.SchedulerEvent;
+import org.openide.filesystems.FileObject;
+import org.openide.util.Lookup;
+
+/**
+ *
+ * @author Miloš Pensimus
+ */
+public class AddFileDefinitionToContextTask extends ParserResultTask<ParsingResult> {
+
+    public AddFileDefinitionToContextTask() {
+    }
+
+    @Override
+    public void run(ParsingResult result, SchedulerEvent event) {
+        ICoffeeScriptMainContext context = Lookup.getDefault().lookup(ICoffeeScriptMainContext.class);
+        FileObject fo = result.getSnapshot().getSource().getFileObject();
+        Lookup lookup = fo.getLookup();
+        CoffeeScriptDataObject dataObject = lookup.lookup(CoffeeScriptDataObject.class);
+        if(dataObject != null) {
+            dataObject.publishGrammarDescriptor(result.getGd());
+        }
+        context.addDefinition(fo, result.getGd());
+    }
+
+    @Override
+    public int getPriority() {
+        return 100;
+    }
+
+    @Override
+    public Class<? extends Scheduler> getSchedulerClass() {
+        return Scheduler.EDITOR_SENSITIVE_TASK_SCHEDULER;
+    }
+
+    @Override
+    public void cancel() {
+    }
+}
