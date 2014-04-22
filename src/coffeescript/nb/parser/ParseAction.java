dac1e0cc4f1ff904fd13a33733d437ce503985c8
milos <milos@milos-R500>
2014-04-22 18:28:18 +0200

    * updated ParseAction

--- src/coffeescript/nb/parser/ParseAction.java
+++ src/coffeescript/nb/parser/ParseAction.java
@@ -4,22 +4,21 @@
  */
 package coffeescript.nb.parser;
 
+import coffeescript.nb.antlr.parser.ParserErrorListener;
 import coffeescript.lexer.CoffeeScriptNativeLexer;
 import coffeescript.lexer.CoffeeScriptNativeLexerException;
 import coffeescript.nb.antlr.parser.CoffeeScriptParserGrammar;
-import coffeescript.nb.antlr.parser.CoffeeScriptTokenStream;
-import coffeescript.nb.antlr.parser.ParseTreeListener;
-import coffeescript.nb.antlr.parser.definitions.BlockDefinition;
-import coffeescript.nb.antlr.parser.definitions.ClassDefinition;
+import coffeescript.nb.antlr.parser.CoffeeScriptParseTreeListener;
+import coffeescript.nb.antlr.parser.CoffeeScriptParserTokenSource;
 import coffeescript.nb.core.CoffeeScriptLanguage;
 import coffeescript.nb.antlr.parser.definitions.CoffeeScriptFileDefinition;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.Callable;
 import javax.swing.SwingUtilities;
 import javax.swing.text.StyledDocument;
-import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenSource;
 import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.atn.PredictionMode;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.netbeans.modules.csl.spi.ParserResult;
@@ -43,21 +42,28 @@ public class ParseAction implements Callable<ParserResult> {
 
         try {
             CoffeeScriptNativeLexer lexer = new CoffeeScriptNativeLexer(snapshot.getText().toString());
-            TokenStream tokens = new CoffeeScriptTokenStream(lexer.tokenize(true), CoffeeScriptLanguage.getTypeMap());               
+            TokenSource tokenSource = new CoffeeScriptParserTokenSource(lexer.tokenize(true), CoffeeScriptLanguage.getTypeMap());       
+            TokenStream tokens = new CommonTokenStream(tokenSource);
             CoffeeScriptParserGrammar parser = new CoffeeScriptParserGrammar(tokens);
             parser.removeErrorListeners();  
             parser.addErrorListener(listener);
+            parser.getInterpreter().setPredictionMode(PredictionMode.LL);
+            parser.setTrimParseTree(true);
             ParseTree tree = parser.root();  
-            List<ClassDefinition> classes = new ArrayList<ClassDefinition>();
-            BlockDefinition rootBlock = new BlockDefinition();
-            ParseTreeWalker walker = new ParseTreeWalker();
-            final StyledDocument doc = (StyledDocument) snapshot.getSource().getDocument(false);
-            walker.walk(new ParseTreeListener(classes, rootBlock, doc), tree);
-            grammarDesc.setRootBlock(rootBlock);
-            grammarDesc.setClasses(classes);
+            ParseTreeWalker walker = new ParseTreeWalker();            
+            final StyledDocument doc = (StyledDocument) snapshot.getSource().getDocument(true);
+            CoffeeScriptParseTreeListener parseTreeListener = new CoffeeScriptParseTreeListener(doc);
+            walker.walk(parseTreeListener, tree);
+            
+            grammarDesc.setRootClasses(parseTreeListener.getRootClasses());
+            grammarDesc.setRootFields(parseTreeListener.getRootVariables());
+            grammarDesc.setRootMethods(parseTreeListener.getRootMethods());
+            grammarDesc.setFields(parseTreeListener.getVariables());
+            grammarDesc.setMethods(parseTreeListener.getMethods());
+            
             SwingUtilities.invokeLater(new AutocompilationTask(snapshot));
         } catch (CoffeeScriptNativeLexerException e) {            
-            listener.addSingleError(e.getLine() + 1, e.getColumn() + 1, e.getMessage());
+            listener.addSingleError(e.getLine(), e.getColumn(), e.getMessage());
         } catch (Exception ex) {
             Exceptions.printStackTrace(ex);
         } finally {
